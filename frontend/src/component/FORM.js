ea2581c494bf343ad873d8ad6a77c32f475399b8
Prashant Srivastav <srivastavp891@gmail.com>
2023-10-15 02:18:08 +0530

    fetch change and dispaly

--- frontend/src/component/FORM.js
+++ frontend/src/component/FORM.js
@@ -1,41 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function FORM() {
+  const [formData, setFormData] = useState([]);
+
+  useEffect(() => {
+    // Fetch the data from your server
+    fetchData()
+  }, []);
+
+  const fetchData = () => {
+    fetch("http://localhost:5000/api/get/form")
+    .then((response) => response.json())
+    .then((data) => {
+      setFormData(data.forms);
+    })
+    .catch((error) => {
+      console.error("Error fetching form data: ", error);
+    });
+  }
+
+  const handlePostButtonClick = (id) => {
+    // Send a PUT request to update the 'posted' status
+    fetch(`http://localhost:5000/api/update/form/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ posted: true }),
+    })
+      .then(() => {
+        // Update the 'formData' state to reflect the change
+        fetchData()
+        setFormData((prevData) =>
+          prevData.map((form) =>
+            form._id === id ? { ...form, posted: true } : form
+          )
+        );
+      })
+      .catch((error) => {
+        console.error("Error updating form data: ", error);
+      });
+  };
+
   return (
     <div>
-      <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-wrap -m-12">
-            {
-              <div class="p-12 md:w-1/2 flex flex-col items-start">
-                <a class="inline-flex items-center">
-                  <img
-                    alt="blog"
-                    src="https://dummyimage.com/104x104"
-                    class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                  />
-                  <span class="flex-grow flex flex-col pl-4">
-                    <span class="title-font font-medium text-gray-900">
-                      Holden Caulfield
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-12 justify-center">
+            {formData.map((form, index) => (
+              <div
+                className="p-5 md:w-1/2 flex flex-col items-start text-white"
+                key={index}
+              >
+                <div className="bg-gray-800 w-full p-12">
+                  <a className="inline-flex items-center">
+                    <img
+                      alt="blog"
+                      src="https://dummyimage.com/104x104"
+                      className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+                    />
+                    <span className="flex-grow flex flex-col pl-4">
+                      <span className="title-font font-medium text-white">
+                        ramswaroopconfession2.0
+                      </span>
+                      <span className="text-orange-600 text-xs tracking-widest mt-0.5">
+                        {new Date(form.timestamp).toLocaleString()}
+                      </span>
                     </span>
-                    <span class="text-gray-400 text-xs tracking-widest mt-0.5">
-                      UI DEVELOPER
-                    </span>
-                  </span>
-                </a>
-                <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">
-                  Roof party normcore before they sold out, cornhole vape
-                </h2>
-                <p class="leading-relaxed mb-8">
-                  Live-edge letterpress cliche, salvia fanny pack humblebrag
-                  narwhal portland. VHS man braid palo santo hoodie brunch trust
-                  fund. Bitters hashtag waistcoat fashion axe chia unicorn.
-                  Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug
-                  you probably haven't heard of them hexagon kickstarter craft
-                  beer pork chic.
-                </p>
+                  </a>
+                  
+                  <hr />
+                  <p className="leading-relaxed mb-8">{form.text}</p>
+                  <hr/>
+                  <button className=" btn bg-blue-700 p-2 rounded my-3 text-white" onClick={() => handlePostButtonClick(form._id)}>
+                    Mark as Posted
+                  </button>
+                </div>
+                
               </div>
-            }
+            ))}
           </div>
         </div>
       </section>
--- server.js
+++ server.js
@@ -29,28 +29,75 @@ db.once('open', () => {
 // Define a schema for the form data
 const formDataSchema = new mongoose.Schema({
   text: String,
+  posted:Boolean,
   timestamp: Date,
 });
 
 const FormData = mongoose.model('FormData', formDataSchema);
 
-app.post('/submit-form', async (req, res) => {
+// Define a schema for hospital data
+const ModelSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true // This option adds "createdAt" and "updatedAt" fields
+});
+// Create a model for the hospital data using the schema
+const Admin = mongoose.model('Admin', ModelSchema);
+
+app.get('/api/get/form', async (req, res) => {
   try {
-    const { text } = req.body;
-    // Create a new document with the submitted data
-    const formData = new FormData({
-      text,
-      timestamp: new Date(),
-    });
+    const forms = await FormData.find({posted:false})
 
-    // Save the document to the database
-    await formData.save();
-
-    res.status(201).json({ message: 'Form data saved successfully' });
+    res.status(201).json({ forms });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while saving the form data' });
   }
 });
+
+// PUT route to update the 'posted' status
+app.put('/api/update/form/:id', async (req, res) => {
+  try {
+    const formId = req.params.id;
+    const updatedForm = await FormData.findByIdAndUpdate(formId, { posted: true }, { new: true });
+
+    if (!updatedForm) {
+      return res.status(404).json({ error: 'Form not found' });
+    }
+
+    return res.json(updatedForm);
+  } catch (error) {
+    console.error('Error updating form data:', error);
+    return res.status(500).json({ error: 'An error occurred while updating form data' });
+  }
+});
+
+app.post('/create/admin', async (req, res) => {
+  const { email, password } = req.body;
+  const isFound = await Admin.find({ email: email });
+  // console.log(isFound);
+
+  if (isFound.length > 0) { // Check if any user was found
+    res.json({ message: "User already exists with this email." });
+    return;
+  }
+
+  const user = new Admin({
+    email,
+    password
+  });
+
+  await user.save();
+  console.log(user);
+  res.json({ message: "Thanks for signing up", user });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
