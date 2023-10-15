0c20c91a6a86b66f04db91a9e7c86bd4ccf1eacc
Prashant Srivastav <srivastavp891@gmail.com>
2023-10-16 00:53:35 +0530

    implement middleware

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const mongoose = require('mongoose');
 
 // Connect to MongoDB using the environment variable
 mongoose.connect(process.env.MONGODB_URI, {
@@ -26,16 +27,14 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Define a schema for the form data
 const formDataSchema = new mongoose.Schema({
   text: String,
-  posted:Boolean,
+  posted: Boolean,
   timestamp: Date,
 });
 
 const FormData = mongoose.model('FormData', formDataSchema);
 
-// Define a schema for hospital data
 const ModelSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -46,22 +45,40 @@ const ModelSchema = new mongoose.Schema({
     required: true
   },
 }, {
-  timestamps: true // This option adds "createdAt" and "updatedAt" fields
+  timestamps: true
 });
-// Create a model for the hospital data using the schema
+
 const Admin = mongoose.model('Admin', ModelSchema);
+
+// Middleware for user authentication
+const authMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "You must be logged in" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if (err) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+    const { _id } = payload;
+    Admin.findById(_id).then(userData => {
+      req.user = userData;
+      next();
+    });
+  });
+};
 
 app.get('/api/get/form', async (req, res) => {
   try {
-    const forms = await FormData.find({posted:false})
-
-    res.status(201).json({ forms });
+    const forms = await FormData.find({ posted: false });
+    res.status(200).json({ forms });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while saving the form data' });
+    console.error('Error fetching form data:', error);
+    res.status(500).json({ error: 'An error occurred while fetching the form data' });
   }
 });
 
-// PUT route to update the 'posted' status
 app.put('/api/update/form/:id', async (req, res) => {
   try {
     const formId = req.params.id;
@@ -71,33 +88,31 @@ app.put('/api/update/form/:id', async (req, res) => {
       return res.status(404).json({ error: 'Form not found' });
     }
 
-    return res.json(updatedForm);
+    res.status(200).json(updatedForm);
   } catch (error) {
     console.error('Error updating form data:', error);
-    return res.status(500).json({ error: 'An error occurred while updating form data' });
+    res.status(500).json({ error: 'An error occurred while updating form data' });
   }
 });
 
 app.post('/create/admin', async (req, res) => {
-  const { email, password } = req.body;
-  const isFound = await Admin.find({ email: email });
-  // console.log(isFound);
+  try {
+    const { email, password } = req.body;
+    const isFound = await Admin.find({ email: email });
 
-  if (isFound.length > 0) { // Check if any user was found
-    res.json({ message: "User already exists with this email." });
-    return;
+    if (isFound.length > 0) {
+      res.status(400).json({ message: "User already exists with this email." });
+    } else {
+      const user = new Admin({ email, password });
+      await user.save();
+      console.log(user);
+      res.status(201).json({ message: "Thanks for signing up", user });
+    }
+  } catch (error) {
+    console.error('Error creating admin:', error);
+    res.status(500).json({ error: 'An error occurred while creating the admin' });
   }
-
-  const user = new Admin({
-    email,
-    password
-  });
-
-  await user.save();
-  console.log(user);
-  res.json({ message: "Thanks for signing up", user });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
